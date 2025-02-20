aa1bcd10f76e69fd57525788c6564800524ef283
Alfred <59073967+slimer37@users.noreply.github.com>
2025-02-20 00:50:40 -0800

    changable letters

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let isSettingLetter = false;
+/** @type {HTMLCanvasElement} */
+let selectedLetter = null;
+/** @type {HTMLCanvasElement} */
+let infobox;
+
 function loadBoard() {
     const letter = document.getElementById("letter");
 
+    infobox = document.getElementById("infobox");
+
+    infobox.innerText = '';
+
+    letter.firstElementChild.innerText = '';
+
     for (let i = 1; i < 14 * 4; i++) {
-        const newLetter = letter.cloneNode();
+        const newLetter = letter.cloneNode(true);
         document.getElementById("board").appendChild(newLetter);
+
+        setupLetter(newLetter);
 
         if (i == 13 || i == 14 * 3 || i == 14 * 4 - 1) {
             newLetter.style.visibility = 'hidden';
@@ -11,6 +25,62 @@ function loadBoard() {
     }
 
     letter.style.visibility = 'hidden';
+    setupLetter(letter);
+}
+
+function startSettingLetter(target) {
+    if (selectedLetter != null) {
+        selectedLetter.classList.remove("selected-letter");
+    }
+
+    isSettingLetter = true;
+    selectedLetter = target;
+    target.classList.add("selected-letter");
+
+    infobox.innerText = 'Entering letter';
+}
+
+function setupLetter(letter) {
+    letter.onclick = function() {
+        startSettingLetter(letter);
+    }
+
+    letter.classList.add("blank-letter");
+}
+
+function onKeyPress(event) {
+    console.log(event.key)
+
+    if (isSettingLetter && (event.key === "Escape" || event.key === "Backspace")) {
+        stopSettingLetter();
+    }
+
+    if (event.key.length > 1 || !/[a-zA-Z0-9- ]/.test(event.key)) return;
+    
+    if (isSettingLetter && selectedLetter != null) {
+        selectedLetter.firstElementChild.innerText = event.key.toUpperCase();
+
+        if (event.key == ' ') {
+            selectedLetter.classList.add("blank-letter");
+        } else {
+            selectedLetter.classList.remove("blank-letter");
+        }
+
+        stopSettingLetter();
+    }
+}
+
+function stopSettingLetter() {
+    if (selectedLetter != null) {
+        selectedLetter.classList.remove("selected-letter");
+    }
+
+    selectedLetter = null;
+    isSettingLetter = false;
+
+    infobox.innerText = '';
 }
 
 window.onload = loadBoard;
+
+document.onkeydown = onKeyPress;
